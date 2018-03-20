e880af3ca9c2cbc506b48e769030297eca4e3777
mattbramlage <mattbramlage@asana.com>
2018-03-20 14:44:34 -0700

    Some low-hanging lint cleanup

--- examples/print_config.py
+++ examples/print_config.py
@@ -6,13 +6,15 @@ import os
 # we do this so you can try locally without installing.
 sys.path.append(os.path.abspath("../"))
 
-# We show here how the configuration for the app lives inside Asana and is read when the script is run
-
+# We show here how the configuration for the app lives inside Asana and is read
+# when the script is run.
 # This will be a command line app that runs once and exits
-from ohmega.execution_environment.command_line_batch_runner import CommandLineBatchRunner
+from ohmega.execution_environment.command_line_batch_runner \
+    import CommandLineBatchRunner
 
 # This is the project that this script will run against.
-# If there isn't a second ID for a separate project that contains the Asana configuration,
+# If there isn't a second ID for a separate project that contains the Asana
+# configuration,
 # we look in the same project for the config task.
 project_id = 157953484489631
 runner = CommandLineBatchRunner(project_id)
--- ohmega/services/logging_service.py
+++ ohmega/services/logging_service.py
@@ -4,12 +4,18 @@ import sys
 import os
 from enum import IntEnum
 
-# These are flags for standard handler + formatter combos. 
-class BasicLoggingConfiguration(IntEnum):
-    COMMAND_LINE = 1                # Log everything to a command line with reasonable human-readability
-    TWO_SHORT_ROTATING_FILES = 2    # Log a short history suitable for `tail`-ing on a server
-    TEN_MEDIUM_ROTATING_FILES = 4   # Log a more thorough history that saves a longer history.
-    COMMAND_LINE_DEBUG = 8          # A more thorough logging service
+
+# These are flags for standard handler + formatter combos.
+class LoggingConfiguration(IntEnum):
+    # Log everything to a command line with reasonable human-readability
+    COMMAND_LINE = 1
+    # Log a short history suitable for `tail`-ing on a server
+    TWO_SHORT_ROTATING_FILES = 2
+    # Log a longer, more throrough history
+    TEN_MEDIUM_ROTATING_FILES = 4
+    # Debug variant for development on the command line
+    COMMAND_LINE_DEBUG = 8
+
 
 class LoggingService(object):
 
@@ -20,58 +26,59 @@ class LoggingService(object):
         if log_base_path == '':
             # If not, try the current working directory
             log_base_path = os.path.abspath(os.getcwd())
-            # That directory plus "logs/ohmega.log" (which may rotate with a [.N])
+            # That directory plus "logs/ohmega.log"
+            # (which may rotate with a [.N])
         return os.path.join(log_base_path, "logs", "ohmega.log")
 
     @classmethod
     def create_log_directory(klass):
-        return os.makedirs(os.path.dirname(LoggingService.getLogFilename(), 0o644))
-        
+        return os.makedirs(os.path.dirname(
+            LoggingService.getLogFilename(), 0o644))
 
-    def __init__(self, logging_configuration = None):
+    def __init__(self, logging_configuration=None):
 
         logging.getLogger().setLevel(logging.DEBUG)
-        # Note that COMMAND_LINE and None are basically the same thing, meaning that this
-        # configuration will be used by default.
-        if logging_configuration == None or logging_configuration & BasicLoggingConfiguration.COMMAND_LINE:
+        # Note that COMMAND_LINE and None are basically the same thing, meaning
+        # that this configuration will be used by default.
+        if logging_configuration is None or\
+                logging_configuration & LoggingConfiguration.COMMAND_LINE:
             self.add_command_line_logging()
-        if logging_configuration & BasicLoggingConfiguration.COMMAND_LINE_DEBUG:
+        if logging_configuration & LoggingConfiguration.COMMAND_LINE_DEBUG:
             self.add_command_line_debug_logging()
-        # TODO: do an int flag with this, which isn't supported in 2.7
-        #if logging_configuration & BasicLoggingConfiguration.TWO_SHORT_ROTATING_FILES:
-        #    self.addShortRotatingLogging()
-        #if logging_configuration & BasicLoggingConfiguration.TEN_MEDIUM_ROTATING_FILES:
-        #    self.addMediumRotatingLogging()
 
     @property
     def logger(self):
         return logging
 
     def add_command_line_logging(self):
-        formatter = logging.Formatter("%(levelname)-8.8s:%(filename)12.12s@%(lineno)-4.4s %(message)s")
+        formatter = logging.Formatter(
+                "%(levelname)-8.8s:%(filename)12.12s@%(lineno)-4.4s %(message)s")
         stream_handler = logging.StreamHandler(sys.stdout)
         stream_handler.setFormatter(formatter)
         stream_handler.setLevel(logging.INFO)
         logging.getLogger().addHandler(stream_handler)
 
     def add_command_line_debug_logging(self):
-        formatter = logging.Formatter("%(levelname)-8.8s:%(filename)12.12s@%(lineno)-4.4s %(message)s")
+        formatter = logging.Formatter(
+                "%(levelname)-8.8s:%(filename)12.12s@%(lineno)-4.4s %(message)s")
         stream_handler = logging.StreamHandler(sys.stdout)
         stream_handler.setFormatter(formatter)
         stream_handler.setLevel(logging.DEBUG)
         logging.getLogger().addHandler(stream_handler)
 
-    def add_short_rotating_file_logging(self):
-        formatter = logging.Formatter("%(levelname)-8.8s:%(filename)12.12s@%(lineno)-4.4s [%(asctime)-23.23s] $(message)s")
-        LoggingService.make_log_dir()
-        # 2 1-MB log files
-        file_handler = logging.handlers.RotatingFileHandler(LoggingService.getLogFilename(),
-            'a', 1024 * 1024 * 1, 1, 'utf8')
-    
-    def add_medium_rotating_file_logging(self):
-        formatter = logging.Formatter("%(levelname)-8.8s:%(filename)12.12s@%(lineno)-4.4s [%(asctime)-23.23s] $(message)s")
-        LoggingService.make_log_dir()
-        # 10 10-MB log files
-        file_handler = logging.handlers.RotatingFileHandler(LoggingService.getLogFilename(),
-            'a', 1024 * 1024 * 10, 9, 'utf8')
-
+#    def add_short_rotating_file_logging(self):
+#        formatter = logging.Formatter(
+#                "%(levelname)-8.8s:%(filename)12.12s@%(lineno)-4.4s [%(asctime)-23.23s] $(message)s")
+#        LoggingService.make_log_dir()
+#        # 2 1-MB log files
+#        file_handler = logging.handlers.RotatingFileHandler(LoggingService.getLogFilename(),
+#            'a', 1024 * 1024 * 1, 1, 'utf8')
+#    
+#    def add_medium_rotating_file_logging(self):
+#        formatter = logging.Formatter(
+#                "%(levelname)-8.8s:%(filename)12.12s@%(lineno)-4.4s [%(asctime)-23.23s] $(message)s")
+#        LoggingService.make_log_dir()
+#        # 10 10-MB log files
+#        file_handler = logging.handlers.RotatingFileHandler(LoggingService.getLogFilename(),
+#            'a', 1024 * 1024 * 10, 9, 'utf8')
+#
