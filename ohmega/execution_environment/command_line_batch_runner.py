de47d9d9044411c1be50271049a33306fb369039
mattbramlage <mattbramlage@asana.com>
2018-03-21 17:02:21 -0700

    Scooting some files around to where they approximately might live

--- ohmega/execution_environment/command_line_batch_runner.py
+++ ohmega/execution_environment/command_line_batch_runner.py
@@ -8,14 +8,12 @@ from ohmega.services.configuration_service import ConfigurationService
 logger = logging.getLogger(__name__)
 
 
-class CommandLineBatchRunner(object):
+class CommandLineBatchRunner(object): 
     """ The most basic runner - a runner that is called from a command line app
     in a way that's not necessarily meant to use events or webhooks. As with
     other runners, this is the top of the container of state, and will provide
-    access between services to each other (so for instance it holds the logging
-    service's state so that other services have access to it.) All runners take
-    a top level scope for what they pay attention to; at this time, that is
-    only a project.
+    access between services to each other. All runners take a top level scope
+    for what they pay attention to; at this time, that is only a project.
     """
 
     def __init__(self, scope_project_id, configuration_project_id=None):
@@ -43,4 +41,9 @@ class CommandLineBatchRunner(object):
         return self._configuration_service
 
     def run(self):
-        return self._callback_manager.scan_project(self._project_id)
+        """ Run this script in this environment.
+        For a command line batch runner, that simply means to scan all the tasks.
+        """
+        for task in self._client.tasks.find_by_project(
+                self._project_id, fields="id"):
+            self._callback_manager.task_scanned(task[u'id'])
